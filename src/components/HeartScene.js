22ff56c8f4662099e5d07d9b4152c05dbeb97a94
Tu Nguyen <tunguyen@rion-lab.com>
2025-03-07 20:20:30 +0700

    remov eslint err

--- src/components/HeartScene.js
+++ src/components/HeartScene.js
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
 import { gsap } from 'gsap';
 
